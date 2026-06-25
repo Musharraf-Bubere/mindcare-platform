515b2bbd6f9b63d9f9703ace782807ca701a4d08
Musharraf-Bubere <musharrafbubere007@gmail.com>
2026-06-25 16:10:18 +0530

    Connect frontend services with backend API

--- frontend/src/Services/userService.js
+++ frontend/src/Services/userService.js
@@ -1,7 +1,13 @@
-export const loginUser = async (data) => {
-  console.log("Login Data:", data);
+import api from "../api/api";
+
+// Register User
+export const registerUser = async (userData) => {
+  const response = await api.post("/signup", userData);
+  return response.data;
 };
 
-export const registerUser = async (data) => {
-  console.log("Register Data:", data);
+// Login User
+export const loginUser = async (loginData) => {
+  const response = await api.post("/login", loginData);
+  return response.data;
 };
